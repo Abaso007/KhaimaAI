8a37428f6765e62857d7b6b559182fcc9fb56782
Sourcery AI <>
2023-10-05 07:24:40 +0000

    'Refactored by Sourcery'

--- streamlit/amazigh_chatbot.py
+++ streamlit/amazigh_chatbot.py
@@ -6,17 +6,13 @@ import openai
 load_dotenv()
 openai.api_key=os.getenv("OPENAI_API_KEY")
 
-messages = list()
+messages = []
 
 def amazigh_stt(audio_path: str):
     client = Client("https://ayymen-mms-asr.hf.space/")
-    result = client.predict(
-				audio_path,
-                audio_path,
-				"Tachelhit (shi)", 
-				api_name="/predict"
+    return client.predict(
+        audio_path, audio_path, "Tachelhit (shi)", api_name="/predict"
     )
-    return result
 
 def amazigh_tts(text: str):
     client = Client("https://ayymen-amazigh-tts.hf.space/")
@@ -38,14 +34,14 @@ def translate(src: str, src_type:str , dest:str, text:str):
 
     if src=='Tachelhit' and src_type=='speech':
         text=amazigh_stt(text)
-    
+
 
     translation_prompt = f'''Translate me this english text to the {dest} language.
                         The text I will give you to translate is in {src}.
                         I want you to return only the translation. Don't say anything else.
                         I want the translation to be very accurate.'''
 
-    
+
     messages.append(create_message("system", translation_prompt))
 
     try:
@@ -57,10 +53,7 @@ def translate(src: str, src_type:str , dest:str, text:str):
             temperature=1
         )
 
-        translated = completion_response.choices[0].message.content
-    
-        return translated
-    
+        return completion_response.choices[0].message.content
     except Exception as e:
         print("There is an exception in translating the text:\n"+str(e))
 
@@ -69,7 +62,7 @@ def amazigh_resp(text: str):
     resp_prompt = '''Your next response should be in Amazigh Tachelhit language.
                     Your response should be written in the Amazigh Tachelhit language.
                     Do not include any word not in Amazigh Tachelhit'''
-    
+
     messages.append(create_message('system', resp_prompt))
     messages.append(create_message('user', text))
 
@@ -79,19 +72,11 @@ def amazigh_resp(text: str):
             temperature=1
     )
 
-    tachelhit_resp = tachelhit_response.choices[0].message.content
-
-    return tachelhit_resp
+    return tachelhit_response.choices[0].message.content
 
 
 def amazigh_chatbot(req: str, req_type:str):
-    if req_type=='speech':
-        text_amazigh_req = amazigh_stt(req)
-    
-    else:
-        text_amazigh_req = req
-
-
+    text_amazigh_req = amazigh_stt(req) if req_type=='speech' else req
     text_tachelhit_resp = amazigh_resp(text_amazigh_req)
     audio_tachelhit_resp_path = amazigh_tts(text_tachelhit_resp)
 
